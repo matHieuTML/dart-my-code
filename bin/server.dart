1458bdec0216c8957fe132007828efd0e48e54cc
Pako <pako1709@gmail.com>
2024-05-14 16:06:50 +0200

    mise a jour de server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:dotenv/dotenv.dart';
 import 'package:shelf_router/shelf_router.dart';
 import '../routes/content_routes.dart';
 import '../services/bdd_service.dart';
 
 void main(List<String> args) async {
+  // Charge les variables d'environnement depuis le fichier .env
+  var env = DotEnv()..load();
+
   // Initialise le service de base de données
   final databaseService = DatabaseService();
   await databaseService.connect();
@@ -13,7 +17,7 @@ void main(List<String> args) async {
   // Crée une instance de ContentRoutes
   final contentRoutes = ContentRoutes();
 
-  // Configure les routes avec les handlers de ContentRoutes
+  // Les routes
   final handler = Pipeline().addMiddleware(logRequests()).addHandler((request) {
     final router = Router();
 
@@ -28,8 +32,8 @@ void main(List<String> args) async {
     return router(request);
   });
 
-  // Obtenez le port du processus ou utilisez un port par défaut
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  // Port sur lequel le serveur va écouter
+  final port = int.parse(env['PORT'] ?? '8080');
 
   // Démarre le serveur sur toutes les interfaces IP disponibles
   final server = await io.serve(handler, InternetAddress.anyIPv4, port);
