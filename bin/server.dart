0d004ac70f8b98498617f8d88b9c87b0a2efa18c
Abderelk <abderrahmanelkafif@gmail.com>
2024-05-14 14:28:26 +0200

    ajout de la connexion à la bdd.

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -19,6 +20,22 @@ Response _echoHandler(Request request) {
 }
 
 void main(List<String> args) async {
+  // Connect to a MongoDB database.
+  final db = await Db.create(
+      'mongodb+srv://Abder951:Abder2002@cluster0.jngetun.mongodb.net/shareMyCode/');
+  try {
+    await db.open();
+    if (db.isConnected) {
+      print('Connexion à la base de données MongoDB réussie !');
+    } else {
+      print('Échec de la connexion à la base de données MongoDB.');
+    }
+  } catch (e) {
+    print('Erreur lors de la connexion à la base de données MongoDB : $e');
+  } finally {
+    await db.close(); // Fermez toujours la connexion après utilisation
+  }
+
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
@@ -27,7 +44,7 @@ void main(List<String> args) async {
       Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '7456');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }
